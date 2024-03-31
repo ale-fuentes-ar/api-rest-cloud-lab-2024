36f0951ad69e7c28a9bc999c625c4330f3f8f79f
R. Alejandro Fuentes <fuentesra@hotmail.com>
2024-03-31 14:17:56 -0300

    feat - start project

---
+++ src/main/java/edu/alejandro/ApiRestCloudLab2024Application.java
@@ -0,0 +1,13 @@
+package edu.alejandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCloudLab2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestCloudLab2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/alejandro/ApiRestCloudLab2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alejandro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCloudLab2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
